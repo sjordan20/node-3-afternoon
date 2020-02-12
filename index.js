ae35442236e2420c90bafa8fca8fbc4b678369e1
sjordan20 <jordanstephen949@gmail.com>
2020-02-12 15:50:18 -0700

    finished

---
+++ index.js
@@ -0,0 +1,26 @@
+require('dotenv').config()
+const express = require('express')
+const massive = require('massive')
+const productCTRL = require('./product_controller')
+
+const app = express()
+
+const { SERVER_PORT, CONNECTION_STRING } = process.env
+
+massive(CONNECTION_STRING)
+    .then(dbInstance => {
+        app.set('db', dbInstance)
+    })
+    .catch(err => console.log(err))
+
+app.use(express.json())
+
+app.get(`/api/products`, productCTRL.getAll)
+app.get(`/api/products/:id`, productCTRL.getOne)
+app.put(`/api/products/:id`, productCTRL.update)
+app.post(`/api/products`, productCTRL.create)
+app.delete(`/api/products/:id`, productCTRL.delete)
+
+app.listen(SERVER_PORT, () => {
+    console.log(`Server listening on port ${SERVER_PORT}`)
+})
